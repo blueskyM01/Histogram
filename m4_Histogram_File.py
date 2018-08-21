6b48c04e047b01dabf3212024d71481afcd664ef
Jianbing Yang <1226853967@qq.com>
2018-08-21 14:32:52 +0800

    有竞争对流

--- m4_Histogram_File.py
+++ m4_Histogram_File.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import heapq
 import numpy as np
 
-test_list = [2791, 1152, 282, 47, 23, 81, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0]
+test_list = [257, 345, 681, 559, 293, 122, 50, 43, 26, 18, 18, 15, 13, 10, 14, 5, 14, 12, 6, 5, 3, 1, 7, 5, 7, 9, 2, 1,
+             7, 5, 3, 3, 2, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0,
+             1, 0, 0, 0, 0, 0, 1]
 
 maxnum_3 = map(test_list.index, heapq.nlargest(3, test_list))
 maxnum_3_index = list(maxnum_3)
@@ -14,28 +14,28 @@ max1 = test_list[maxnum_3_index[0]]
 max2 = test_list[maxnum_3_index[1]]
 max3 = test_list[maxnum_3_index[2]]
 
-max1_duo = int(max1 / 1.4)
+max1_duo = int(max1 / 6)
 max2_duo = int(max2 / 6)
 max3_duo = int(max3 / 6)
 
 sumduo = max1_duo + max2_duo + max3_duo
-print("sumduo:",sumduo)
+print("sumduo:", sumduo)
 test_list[maxnum_3_index[0]] = max1 - max1_duo
 test_list[maxnum_3_index[1]] = max2 - max2_duo
 test_list[maxnum_3_index[2]] = max3 - max3_duo
 
-cccc = test_list[2:40]
+cccc = test_list[2:36]
 cccclist = []
 already = 0
 for i in range(len(cccc)):
-    ra = np.random.randint(5, (sumduo - already)/5 - (len(cccc) - i), 1, dtype='l')
+    ra = np.random.randint(5, (sumduo - already) / 5 - (len(cccc) - i), 1, dtype='l')
     cccclist.append(ra[0])
-    already =already+ra
+    already = already + ra
 cccclist.append((sumduo - already)[0])
 
-print("sum:",sum(cccclist))
+print("sum:", sum(cccclist))
 print(cccclist)
-for ii, jj in zip(range(2, 40), cccclist):
+for ii, jj in zip(range(2, 36), cccclist):
     test_list[ii] = test_list[ii] + jj
 
 print(test_list)
